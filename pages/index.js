5faea41bf20594e0d489d50f96e2266cc86969c9
marcoderso <104020027+marcoderso@users.noreply.github.com>
2022-05-02 12:23:09 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>dev01d</title>
+        <title>Marc</title>
         <link rel="icon" href="favicon.ico" />
       </Head>
 
